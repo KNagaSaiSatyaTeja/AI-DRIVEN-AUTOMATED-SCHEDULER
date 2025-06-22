cdeaa5148947859a4de08ff0f7b6fab5ed139a42
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-06-22 22:37:42 +0530

    modifed the index to manage all routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const authRoutes = require("./auth");
 const timetableRoutes = require("./timetable");
+const roomRoutes = require("./room");
+const facultyRoutes = require("./faculty");
+const subjectRoutes = require("./subject");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/timetable", timetableRoutes);
+router.use("/rooms", roomRoutes);
+router.use("/faculty", facultyRoutes);
+router.use("/subjects", subjectRoutes);
 
 module.exports = router;
