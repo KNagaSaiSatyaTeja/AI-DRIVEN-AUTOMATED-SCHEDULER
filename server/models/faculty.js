f989987f4c055f867032f1ef36ad44d91b3356bc
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-06-22 22:34:05 +0530

    added the faculty model

---
+++ server/models/faculty.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const timeSlotSchema = new mongoose.Schema({
+  day: {
+    type: String,
+    required: true,
+    enum: [
+      "MONDAY",
+      "TUESDAY",
+      "WEDNESDAY",
+      "THURSDAY",
+      "FRIDAY",
+      "SATURDAY",
+      "SUNDAY",
+      "ALL_DAYS",
+    ],
+  },
+  startTime: { type: String, required: true },
+  endTime: { type: String, required: true },
+});
+
+const facultySchema = new mongoose.Schema({
+  facultyId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  room: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
+  availability: [timeSlotSchema],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Faculty", facultySchema);
